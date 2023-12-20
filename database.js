1977395125cf9028908518cb248f062862f8ad1e
ฮาบีดีน บุญสาลี <habideen@MacBook-Air--Habideen.local>
2023-12-21 01:21:38 +0700

    first commit

---
+++ database.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+const mysql = require("mysql2/promise");
+const dbConn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', // <== ระบุให้ถูกต้อง
+    password: '',  // <== ระบุให้ถูกต้อง
+    database: 'pim_database',
+    port: 3306  // <== ใส่ port ให้ถูกต้อง (default 3306, MAMP ใช้ 8889)
+});
+
+app.get("/students", async (req, res) => {
+    const connection = await dbConn
+    const rows = await connection.query('SELECT * from students')
+    res.send(rows);
+})
+
+app.get("/students/:id", async (req, res) => {
+    const connection = await dbConn
+    const rows = await connection.query('SELECT * from students where id = ' + req.params.id)
+    res.send(rows);
+})
+
+app.delete("/students/:id", async (req, res) => {
+    const connection = await dbConn
+    const rows = await connection.query('SELECT * from students where id = ' + req.params.id)
+    res.send(rows);
+})
+
+app.get("/query-1", async (req, res) => {
+    const connection = await dbConn
+    const rows = await connection.query('SELECT * from students')
+    res.send(rows);
+})
+
+app.get("/query-2", async (req, res) => {
+    const connection = await dbConn
+    const rows = await connection.query('SELECT * from students')
+    res.send(rows);
+})
+
+app.listen(3000, () => {
+    console.log("Server is running at port 3000")
+})
+
